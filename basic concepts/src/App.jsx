5c5e6ab908842da2e3dc195fa0cacd9e8d14d172
Girish <rw3.girish.gk@gmail.com>
2025-02-24 11:34:34 +0530

    useCallback

--- basic concepts/src/App.jsx
+++ basic concepts/src/App.jsx
@@ -8,6 +8,7 @@ import Uncontroll from './Components/Controll/UnControll';
 import Validation from './Components/Validation/Validation';
 import HOC from './Components/HOC/HOC';
 import { Link, Route, Routes, useNavigate } from 'react-router';
+import Callback from './Components/UseCallback/Callback';
 
 let HOCValidate = HOC(Validation)
 let HOCControll = HOC(Controll);
@@ -40,15 +41,16 @@ function App() {
        {/* <Validation /> */}
        {/* <HOCValidate isLoading={isLoading} user="Red & White" /> */}
 
-       <Link to={"/"}>Home</Link> &nbsp;
+       {/* <Link to={"/"}>Home</Link> &nbsp;
        <Link to={"/about"}>About</Link>
         <br />
         <br />
         <br />
         <button onClick={handleCounter}>Counter</button>
-        <button onClick={handleAbout}>About</button>
+        <button onClick={handleAbout}>About</button> */}
        <Routes>
-          <Route path='/' element= {<HOCControll isLoading={isLoading} />} />
+          <Route path='/' element= { <Callback />} />
+          {/* <Route path='/' element= {<HOCControll isLoading={isLoading} />} /> */}
           <Route path='/about/' element= {<h1>About page</h1>} />
           <Route path='/counter/:name' element= {<Counter />} />
           <Route path='/contact' element= {<HOCValidate isLoading={isLoading} user="Red & White" />} />
